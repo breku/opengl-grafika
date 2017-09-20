214c923f1f363ed30b269954305d8e1449c1114c
brekol <brekol@gmail.com>
2017-09-20 21:38:43 +0200

    Zaladowanie modelu z blendera obj.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "common/shader.hpp"
 #include <common/controls.hpp>
 #include <common/texture.hpp>
+#include <vector>
+#include <common/objloader.hpp>
 
 GLFWwindow* window;
 using namespace glm;
@@ -29,7 +31,7 @@ int main( void )
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "Tutorial 0 - Keyboard and Mouse", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "Tutorial 07 - Model Loading", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
@@ -78,101 +80,28 @@ int main( void )
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
     // Load the texture
-    GLuint Texture = loadDDS("/home/brekol/workspaces/clion/tutorial1/uvtemplate.DDS");
+    GLuint Texture = loadDDS("/home/brekol/workspaces/clion/tutorial1/uvmap.DDS");
 
     // Get a handle for our "myTextureSampler" uniform
     GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
-    // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-    static const GLfloat g_vertex_buffer_data[] = {
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f
-    };
+    // Read our .obj file
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec2> uvs;
+    std::vector<glm::vec3> normals; // Won't be used at the moment.
+    bool res = loadOBJ("/home/brekol/workspaces/clion/tutorial1/cube.obj", vertices, uvs, normals);
 
-    // Two UV coordinatesfor each vertex. They were created with Blender.
-    static const GLfloat g_uv_buffer_data[] = {
-            0.000059f, 0.000004f,
-            0.000103f, 0.336048f,
-            0.335973f, 0.335903f,
-            1.000023f, 0.000013f,
-            0.667979f, 0.335851f,
-            0.999958f, 0.336064f,
-            0.667979f, 0.335851f,
-            0.336024f, 0.671877f,
-            0.667969f, 0.671889f,
-            1.000023f, 0.000013f,
-            0.668104f, 0.000013f,
-            0.667979f, 0.335851f,
-            0.000059f, 0.000004f,
-            0.335973f, 0.335903f,
-            0.336098f, 0.000071f,
-            0.667979f, 0.335851f,
-            0.335973f, 0.335903f,
-            0.336024f, 0.671877f,
-            1.000004f, 0.671847f,
-            0.999958f, 0.336064f,
-            0.667979f, 0.335851f,
-            0.668104f, 0.000013f,
-            0.335973f, 0.335903f,
-            0.667979f, 0.335851f,
-            0.335973f, 0.335903f,
-            0.668104f, 0.000013f,
-            0.336098f, 0.000071f,
-            0.000103f, 0.336048f,
-            0.000004f, 0.671870f,
-            0.336024f, 0.671877f,
-            0.000103f, 0.336048f,
-            0.336024f, 0.671877f,
-            0.335973f, 0.335903f,
-            0.667969f, 0.671889f,
-            1.000004f, 0.671847f,
-            0.667979f, 0.335851f
-    };
+    // Load it into a VBO
 
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
 
     GLuint uvbuffer;
     glGenBuffers(1, &uvbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
 
     do{
 
@@ -203,7 +132,7 @@ int main( void )
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glVertexAttribPointer(
-                0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+                0,                  // attribute
                 3,                  // size
                 GL_FLOAT,           // type
                 GL_FALSE,           // normalized?
@@ -215,8 +144,8 @@ int main( void )
         glEnableVertexAttribArray(1);
         glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
         glVertexAttribPointer(
-                1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-                2,                                // size : U+V => 2
+                1,                                // attribute
+                2,                                // size
                 GL_FLOAT,                         // type
                 GL_FALSE,                         // normalized?
                 0,                                // stride
@@ -224,7 +153,7 @@ int main( void )
         );
 
         // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
 
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
@@ -241,7 +170,7 @@ int main( void )
     glDeleteBuffers(1, &vertexbuffer);
     glDeleteBuffers(1, &uvbuffer);
     glDeleteProgram(programID);
-    glDeleteTextures(1, &TextureID);
+    glDeleteTextures(1, &Texture);
     glDeleteVertexArrays(1, &VertexArrayID);
 
     // Close OpenGL window and terminate GLFW
